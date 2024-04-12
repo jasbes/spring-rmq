2dda54092db2edb32125c3ef600cae40b33685e8
Jefferson <jefferson_uk@hotmail.com>
2024-04-12 22:03:57 +0200

    First commit

---
+++ src/test/java/com/rmq/springrmq/SpringRmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmq.springrmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
